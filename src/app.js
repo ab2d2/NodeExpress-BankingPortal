11569c4328362b615dceb585f8a98cf1126686ee
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-25 22:45:45 +0100

    add more routes + use accounts data

--- src/app.js
+++ src/app.js
@@ -10,7 +10,18 @@ app.set('view engine', 'ejs');
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
 app.use('/css', express.static(path.join(__dirname, 'public/css')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Account Summary' }));
+const accountData = fs.readFileSync(path.resolve(__dirname, 'json/accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.resolve(__dirname, 'json/users.json'), 'utf8');
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
 
 
 app.listen(port, () => {
