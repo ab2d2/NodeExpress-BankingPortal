f62b3dcd8d644b73bb678a7619228acda126571e
ab2d2 <abdul.rahimi@capgemini.com>
2020-08-25 23:15:00 +0100

    refactor

--- src/app.js
+++ src/app.js
@@ -15,27 +15,33 @@ app.use(express.urlencoded(true));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
-app.post('/transfer', (req, res) => {
-  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-  writeJSON();
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+// app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+// app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+// app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-  res.render('transfer', { message: 'Transfer Completed' });
-});
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount, 10);
+// app.get('/transfer', (req, res) => res.render('transfer'));
+// app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
 
-  writeJSON();
-  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
-});
+// app.post('/transfer', (req, res) => {
+//   accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
+//   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+//   writeJSON();
+
+//   res.render('transfer', { message: 'Transfer Completed' });
+// });
+// app.post('/payment', (req, res) => {
+//   accounts.credit.balance -= req.body.amount;
+//   accounts.credit.available += parseInt(req.body.amount, 10);
+
+//   writeJSON();
+//   res.render('payment', { message: 'Payment Successful', account: accounts.credit });
+// });
 
 app.listen(port, () => {
   console.log(`Server is up and running on port :${port}`);
